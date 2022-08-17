fc0b4b2c7cabbda895cf83f2769aaae33aaadc78
Natalya Pykhtina <sovanst4@yandex.ru>
2022-08-17 09:27:56 +0300

    Zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число N: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = 1;
+while (number2 <= number1)
+{
+     if (number2 % 2 == 0)     
+     {
+        Console.WriteLine(number2);
+     }
+        number2 = number2 + 1;
+}
